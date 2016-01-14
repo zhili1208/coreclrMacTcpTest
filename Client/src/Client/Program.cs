12fa39d97064f9c9622f529bc7eb8f997341d224
NuGetCI Agent <nugetciagent@nugetci-a03.guest.corp.microsoft.com>
2016-01-14 14:36:23 -0800

    more info

--- Client/src/Client/Program.cs
+++ Client/src/Client/Program.cs
@@ -11,12 +11,18 @@ namespace Client
     {
         public static void Main(string[] args)
         {
+            Console.WriteLine("Client start");
             var client = new TcpClient();
             client.Connect("127.0.0.1", 55555);
+            
+            Console.WriteLine("Connecting");
             var stream = client.GetStream();
             var writer = new StreamWriter(stream);
-
+            
+            Console.WriteLine("Connected");
+            
             writer.WriteLine("test");
+            Console.WriteLine("write to network stream");
             writer.Flush();
 
             Console.WriteLine("sended");
