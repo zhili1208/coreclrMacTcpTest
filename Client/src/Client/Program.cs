9f4ea3622a310c1580c643e258baedfd988e070f
Zhi Li <zhili128@outlook.com>
2016-01-14 14:05:38 -0800

    test app

---
+++ Client/src/Client/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.IO;
+
+namespace Client
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var client = new TcpClient();
+            client.Connect("127.0.0.1", 55555);
+            var stream = client.GetStream();
+            var writer = new StreamWriter(stream);
+
+            writer.WriteLine("test");
+            writer.Flush();
+
+            Console.WriteLine("sended");
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Net;
+using System.IO;
+
+namespace Server
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int port = 55555;
+            var listener = new TcpListener(IPAddress.Loopback, port);
+            listener.Start();
+
+            var client = listener.AcceptTcpClient();
+            var stream = client.GetStream();
+
+            var reader = new StreamReader(stream);
+
+            Console.WriteLine(reader.ReadLine());
+        }
+    }
+}
